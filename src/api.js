bd2d809d4c34b43bb15718b436270270695b1ef7
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-03-05 09:53:35 -0500

    Adding AWS Controller and data files, plus route update

--- src/api.js
+++ src/api.js
@@ -1,21 +1,28 @@
+// 3rd party libraries
 const express = require('express');
 const serverless = require('serverless-http');
 
-const app = express();
+// Self Written Modules
+const aws = require('./controllers/aws')
 
+// Express Parameters
+const app = express();
 const router = express.Router();
 
+// Routes
 router.get('/', (req, res) => {
     res.json({
-        'hello' : 'h1'
+        'name' : 'Cedar Property Advisors API',
+        'version' : '1.0.0',
+        'author' : 'Brandon Hiles'
     });
 });
+router.get('/upload', (req, res) => res.send(aws.upload('./src/controllers/data/listingsData.json')))
+router.get('/data', (req, res) => {
+    let data = aws.grabData('Listings')
+    data.then(result => res.json(result))
+})
 
-router.get('/test', (req, res) => {
-    res.json({
-        'hello' : 'test'
-    });
-});
 
 app.use('/.netlify/functions/api', router)
 
