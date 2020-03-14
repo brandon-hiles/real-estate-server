118bc149225588cf64e0f98e60c7e8e01093c686
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-03-14 18:07:19 -0400

    Adding routes and comments

--- src/api.js
+++ src/api.js
@@ -11,19 +11,32 @@ const router = express.Router();
 
 // Routes
 router.get('/', (req, res) => {
+    // Root Route: Display information about our server
     res.json({
         'name' : 'Cedar Property Advisors API',
         'version' : '1.0.0',
         'author' : 'Brandon Hiles'
     });
 });
-router.get('/upload', (req, res) => res.send(aws.upload('./src/controllers/data/listingsData.json')))
+router.get('/upload', (req, res) => {
+    // Upload Route: Upload our data file to AWS 
+    res.send(aws.upload('./src/controllers/data/listingsData.json'))
+})
 router.get('/data', (req, res) => {
+    // Display Route: Display our data from DynamboDB Database
     let data = aws.grabData('Listings')
     data.then(result => res.json(result))
 })
+router.get('/data/:number', (req, res) => {
+    // Filtered Data Route: Display number amount of results from DynamboDB
+    let data = aws.grabData('Listings')
+    data.then(result => {
+        let filteredData = result.slice(0, req.params.number)
+        res.json(filteredData)
+    })
+})
 
-
+// Netlfiy configurations
 app.use('/.netlify/functions/api', router)
 
 module.exports.handler = serverless(app);
